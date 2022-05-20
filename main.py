c65d82cd9a4fe786bfe953c0cc5b9421a65a2185
Fred Hsieh <fhsieh@uci.edu>
2022-05-20 00:33:38 -0700

    Remove time library and change sleep time to 2(test)

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import asyncio
 import logging
 import os
 import re
-import time
 from datetime import datetime
 from logging.handlers import RotatingFileHandler
 
@@ -248,7 +247,7 @@ class MyClient(discord.Client):
                         logging.info("Successfully sent message to channel")
 
                     # Check if this message has embed again (if true, delete the sent webhook)
-                    await asyncio.sleep(5)
+                    await asyncio.sleep(2)
 
                     if message.embeds:
                         embed_url_list = [embed.url for embed in message.embeds]
