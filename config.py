1b256328d4adfdea50543c8d39d78450daa3f258
Fred Hsieh <fhsieh@uci.edu>
2022-05-23 17:03:35 -0700

    Change archi of codes

--- config.py
+++ config.py
@@ -1,3 +1,15 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+
+from dotenv import load_dotenv
+
+import discord_client
+import twitter_client
+
+AVATAR_IMG = "./avatar.jpg"  # default image for webhook
+LOG_NAME = "twitfix.log"
+
 mp4_list = [
     "https://i.imgur.com/cD9zcNa.mp4",
     "https://i.imgur.com/rMGBqDw.mp4",
@@ -19,3 +31,75 @@ cmd_table = {
     "get_avatar": "Get current webhook avatar",
     "get_name": "Get current webhook name ",
 }
+
+
+class GLOBAL:
+    def __init__(
+        self,
+        LOGGER=None,
+        BOT_NAME="",
+        WEBHOOK_NAME="",
+        WEBHOOK_AVATAR=None,
+        mp4_list=[],
+        gif_list=[],
+        cmd_table={},
+        TWITTER_CLI=None,
+        DISCORD_TOKEN="",
+        DISCORD_CLI=None,
+    ):
+        # Log-related
+        self.LOGGER = LOGGER
+        # Webhook-related
+        self.BOT_NAME = BOT_NAME
+        self.WEBHOOK_NAME = WEBHOOK_NAME
+        self.WEBHOOK_AVATAR = WEBHOOK_AVATAR
+
+        self.mp4_list = mp4_list
+        self.gif_list = gif_list
+        self.cmd_table = cmd_table
+
+        # Client
+        self.TWITTER_CLI = TWITTER_CLI
+        self.DISCORD_TOKEN = DISCORD_TOKEN
+        self.DISCORD_CLI = DISCORD_CLI
+
+
+def init():
+    log_format = "%(asctime)s [%(levelname)s]: %(message)s"
+    logging.basicConfig(
+        format=log_format,
+        datefmt="%m/%d/%Y %I:%M:%S %p",
+        level=logging.INFO,
+        handlers=[
+            logging.StreamHandler(),
+            logging.FileHandler(LOG_NAME, mode="w"),
+            RotatingFileHandler(LOG_NAME, maxBytes=5 * 1024, backupCount=2),
+        ],
+    )
+    LOGGER = logging.getLogger(__name__)
+
+    load_dotenv()
+
+    BOT_NAME = os.environ.get("BOT_NAME")
+    WEBHOOK_NAME = os.environ.get("WEBHOOK_NAME")
+    fp = open(AVATAR_IMG, "rb")
+    WEBHOOK_AVATAR = fp.read()
+    TWITTER_CLI = twitter_client.TwitterClient()
+
+    DISCORD_TOKEN = os.environ.get("DISCORD_CLIENT_TOKEN")
+
+    GLOBAL_SETTINGS = GLOBAL(
+        LOGGER=LOGGER,
+        BOT_NAME=BOT_NAME,
+        WEBHOOK_NAME=WEBHOOK_NAME,
+        WEBHOOK_AVATAR=WEBHOOK_AVATAR,
+        mp4_list=mp4_list,
+        gif_list=gif_list,
+        cmd_table=cmd_table,
+        TWITTER_CLI=TWITTER_CLI,
+        DISCORD_TOKEN=DISCORD_TOKEN,
+    )
+
+    DISCORD_CLI = discord_client.DiscordClient(GLOBAL_SETTINGS)
+
+    return GLOBAL_SETTINGS, DISCORD_CLI
---
+++ discord_client.py
@@ -0,0 +1,271 @@
+import asyncio
+import random
+
+import discord
+import requests
+from discord_webhook import DiscordWebhook
+
+from discord_message import *
+from twitter_client import *
+
+REQUEST_SUCCESS_CODE = {200, 204}
+re_status = re.compile("\\w{1,15}\\/(status|statuses)\\/\\d{2,20}")
+
+
+class DiscordClient(discord.Client):
+    def __init__(self, GLOBAL):
+        super().__init__()
+        self.LOGGER = GLOBAL.LOGGER
+        self.BOT_NAME = GLOBAL.BOT_NAME
+        self.WEBHOOK_NAME = GLOBAL.WEBHOOK_NAME
+        self.WEBHOOK_AVATAR = GLOBAL.WEBHOOK_AVATAR
+        # This is used for changing webhook avatar when posting
+        self.WEBHOOK_AVATAR_URL = None
+        self.cmd_table = GLOBAL.cmd_table
+        self.gif_list = GLOBAL.gif_list
+        self.mp4_list = GLOBAL.mp4_list
+
+        self.TWITTER_CLI = GLOBAL.TWITTER_CLI
+
+    async def on_ready(self):
+        self.LOGGER.info("Logged on as {0}!".format(self.user))
+        await self.change_presence(activity=discord.Game(self.WEBHOOK_NAME))
+
+    async def get_webhook(self, message):
+        channel_all_webhooks = await message.channel.webhooks()
+        send_webhook = None  # this is used for getting webhook url (for later usage)
+        if len(channel_all_webhooks) == 0:
+            # create webhook for twitter-fix bot
+            send_webhook = await message.channel.create_webhook(
+                name=self.BOT_NAME, avatar=self.WEBHOOK_AVATAR
+            )
+        else:
+            send_webhook = discord.utils.get(channel_all_webhooks, name=self.BOT_NAME)
+            if send_webhook is None:
+                # need to create a new webhook for this app
+                send_webhook = await message.channel.create_webhook(
+                    name=self.BOT_NAME, avatar=self.WEBHOOK_AVATAR
+                )
+
+        webhook_url = send_webhook.url
+        if send_webhook is None or webhook_url is None:
+            self.LOGGER.error("Failed to fetch webhook")
+            return
+
+        webhook = DiscordWebhook(
+            url=webhook_url,
+            rate_limit_retry=True,
+            username=self.WEBHOOK_NAME,
+            avatar_url=self.WEBHOOK_AVATAR_URL if self.WEBHOOK_AVATAR_URL else None,
+        )
+
+        return webhook
+
+    async def print_help(self, channel):
+        help_msg = discord.Embed(title="DM me with command! #help", color=0xFFB8D1)
+        for cmd_name, cmd_desc in self.cmd_table.items():
+            help_msg.add_field(name="#%s" % cmd_name, value=cmd_desc, inline=False)
+
+        display_thumb = self.gif_list[random.randint(0, len(self.gif_list) - 1)]
+        help_msg.set_thumbnail(url=display_thumb)
+        await channel.send(embed=help_msg)
+
+    async def fun(self, channel):
+        await channel.send(self.mp4_list[random.randint(0, len(self.gif_list) - 1)])
+
+    async def handle_dm_message(self, message):
+        msg_list = message.content.split()
+        if msg_list[0].startswith("#"):
+            cmd = msg_list[0][1:]
+            if cmd == "help":
+                await self.print_help(message.channel)
+            elif cmd == "set_avatar":
+                self.LOGGER.info(
+                    "User {} changed webhook avatar to {}.".format(
+                        message.author, msg_list[1]
+                    )
+                )
+                self.WEBHOOK_AVATAR_URL = msg_list[1]
+                await message.channel.send(
+                    "Changed webhook avatar to <%s>" % self.WEBHOOK_AVATAR_URL
+                )
+            elif cmd == "set_name":
+                global WEBHOOK_NAME
+                self.LOGGER.info(
+                    "User {} changed webhook name from {} to {}".format(
+                        message.author, WEBHOOK_NAME, msg_list[1]
+                    )
+                )
+                WEBHOOK_NAME = msg_list[1]
+                await message.channel.send("Changed webhook name to " + WEBHOOK_NAME)
+                # Change now playing
+                await self.change_presence(activity=discord.Game(WEBHOOK_NAME))
+            elif cmd == "get_avatar":
+                if self.WEBHOOK_AVATAR_URL:
+                    await message.channel.send(
+                        "Current webhook avatar is " + self.WEBHOOK_AVATAR_URL
+                    )
+                else:
+                    await message.channel.send(
+                        "Current webhook avatar is default image"
+                    )
+            elif cmd == "get_name":
+                if WEBHOOK_NAME:
+                    await message.channel.send(
+                        "Current webhook name is " + WEBHOOK_NAME
+                    )
+                else:
+                    await message.channel.send("Current webhook name is default name")
+            else:
+                await self.fun(message.channel)
+
+        else:
+            await self.print_help(message.channel)
+
+    async def on_message(self, message):
+        # check if user sends a message
+        if message.author.bot or message.author == self.user:
+            return
+
+        # Check if this is a dm message
+        if not message.guild:
+            await self.handle_dm_message(message)
+            return
+
+        # Check if user mentions this bot
+        if self.user in message.mentions:
+            await self.fun(message.channel)
+            return
+
+        message_list = message.content.split()
+        if message.content.startswith("||") and message.content.endswith("||"):
+            # this is a spoiler message, skip
+            self.LOGGER.info("Spoiler message, skipping")
+            return
+
+        embeds_list = []
+        if message.embeds:
+            for embed in message.embeds:
+                embeds_list.append(embed.url)  # urls that have embed, should be skipped
+
+        for msg in message_list:
+            is_tweet = self.is_valid_twitter_url(msg)
+            if is_tweet is not None:
+                await self.handle_twitter_message(is_tweet, message)
+            else:
+                # This is not a tweet url, skipping...
+                continue
+
+    async def handle_twitter_message(self, is_tweet, message):
+        # Valid tweet found
+        twitter_url = is_tweet
+        self.LOGGER.info("Tweet Found: {}".format(twitter_url))
+        # Try to fetch tweet object
+        tweet = Tweet(twitter_url, self.TWITTER_CLI)
+        if tweet is not None:  # A valid message found!
+            if tweet.type == "Image":
+                if not tweet.is_hidden():
+                    self.LOGGER.info("This is not a hidden tweet... Skipping")
+                    # no need to post this image
+                    return
+                self.LOGGER.info("Hidden image found... Start processing")
+                tweet.download_image()
+
+            elif tweet.type == "Video":
+                if not tweet.is_hidden():
+                    self.LOGGER.info("This is not a hidden tweet... Skipping")
+                    # no need to post this video/gif
+                    return
+                self.LOGGER.info("Hidden video/gif found... Start processing")
+                tweet.download_video()
+
+            elif tweet.type == "Text":
+                return  # not implemented yet
+
+            else:
+                self.LOGGER.warning("Failed to process, not a valid tweet?")
+                return  # not implemented yet
+
+            # Build tweet info (author, date, url...)
+            tweet.fetch_info()
+            # get webhook to post this tweet
+            webhook = await self.get_webhook(message)
+
+            tweet_output = tweet.output()
+
+            # add content to webhook message
+            message_content = DiscordMessage(tweet_output, logger=self.LOGGER)
+            if tweet.type == "Image":
+                embed_list = message_content.embed_list
+                for embed in embed_list:
+                    webhook.add_embed(embed)
+
+            elif tweet.type == "Video":
+                video = message_content.video
+                tweet_info = message_content.embed_list[0]
+                # Send tweet info first
+                webhook.add_embed(tweet_info)
+                info_wh = webhook.execute()
+                webhook.remove_embeds()
+                if info_wh.status_code not in REQUEST_SUCCESS_CODE:
+                    self.LOGGER.warning("Failed to sent video tweet info!")
+
+                # Add video url to content, which will be sent later
+                webhook.set_content(video["url"])
+
+            # send message to this channel!
+            sent_webhook = webhook.execute()
+            # check status code
+            if sent_webhook.status_code not in REQUEST_SUCCESS_CODE:
+                self.LOGGER.warning("Failed to sent message webhook!")
+            else:
+                self.LOGGER.info("Successfully sent message to channel")
+
+            # Check if this message has embed again (if true, delete the sent webhook)
+            if tweet.type == "Image":
+                await asyncio.sleep(2)
+                if message.embeds:
+                    embed_table = dict(
+                        [(embed.url, embed.image) for embed in message.embeds]
+                    )
+
+                    if (
+                        tweet.url in embed_table
+                        and embed_table[tweet.url].url != discord.Embed.Empty
+                    ):
+                        # Check if message embed contains image
+                        # delete latest image from bot
+                        self.LOGGER.info(
+                            "Deleting sent webhook, previous message has embed (should not be sent)"
+                        )
+                        webhook.delete(sent_webhook)
+
+                    else:
+                        # no duplicate embed image found, continue...
+                        return
+
+        else:
+            # Not a valid tweet
+            self.LOGGER.warning("Failed to process this tweet")
+            return
+
+    def is_valid_twitter_url(self, url):
+        if "twitter.com" not in url:
+            # Not a valid url, skipping
+            return None
+
+        match = re_status.search(url)
+        if match is not None:
+            twitter_url = url
+            if match.start() == 0:
+                twitter_url = "https://twitter.com/" + url
+            # try to request this url and check whether it is a valid tweet
+            if not url.startswith("https://twitter.com"):
+                # not a valied tweet! (status found, but might not be a twitter url)
+                return None
+            status_code = requests.get(twitter_url)
+            if status_code not in REQUEST_SUCCESS_CODE:
+                return twitter_url
+            else:
+                return None
+        return None
